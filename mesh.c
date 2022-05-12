e878adbab289ad1fa9cdecc2b8a7a9e17664aa0e
elmfrain <elmfer10@gmail.com>
2022-05-12 00:16:49 -0500

    mesh.c: removed ability to load BIN mesh files
    website used a BIN file to load mod logo mesh (just like in the actual mod), but loading from known file types is more easier and stright forward using Assimp

--- mesh.c
+++ mesh.c
@@ -31,21 +31,10 @@ static Mesh* i_lAdd(Mesh mesh);
 static void i_lRemove(Mesh* mesh);
 
 static void i_meshLoadAssimp(const char* filename);
-static void i_meshLoadBIN(const char* filename);
 
 void prwmLoad(const char* filename)
 {
-    char extension[24] = { 0 };
-    for(int i = strlen(filename) - 1; i >= 0; i--)
-        if(filename[i] == '.')
-        {
-            strcpy(extension, filename + i + 1);
-            break;
-        }
-
-    if(strcmp(extension, "bin") == 0) 
-        i_meshLoadBIN(filename);
-    else i_meshLoadAssimp(filename);
+    i_meshLoadAssimp(filename);
 }
 
 PRWmesh* prwmMeshGet(const char* meshName)
@@ -249,84 +238,4 @@ static void i_meshLoadAssimp(const char* filename)
 
         i_lAdd(newMesh);
     }
-}
-
-static void i_meshLoadBIN(const char* filename)
-{
-    struct MeshHeader
-    {
-        int verticiesArrayPtr, verticiesArraySize;
-	    int indiciesArrayPtr, indiciesArraySize;
-	    int texturePtr, textureWidth, textureHeight;
-    } MeshHeader;
-
-    FILE* file = fopen(filename, "rb");
-
-    if(file)
-    {
-        fseek(file, 0, SEEK_END);
-        size_t fileSize = ftell(file);
-
-        char* bufferedInput = malloc(fileSize);
-        rewind(file);
-        size_t ret = fread((char*) bufferedInput, fileSize, 1, file);
-        fclose(file);
-
-        char* bufferReadPtr = bufferedInput;
-    createMesh:
-        {
-            if(!*bufferReadPtr)
-            {
-                free(bufferedInput);
-                return;
-            }
-
-            Mesh mesh;
-            struct MeshHeader header;
-            memset(mesh.name, 0, sizeof(mesh.name));
-            mesh.vertexFormat = PRWM_VERTF_POS_UV;
-
-            strncpy(mesh.name, bufferReadPtr, sizeof(mesh.name) - 1);
-            bufferReadPtr += strlen(bufferReadPtr) + 1;
-
-            memcpy(&header.verticiesArrayPtr, bufferReadPtr, sizeof(int32_t) * 7);
-            bufferReadPtr += sizeof(int32_t) * 7;
-
-            const char* vertexData = bufferedInput + header.verticiesArrayPtr;
-            const char* indexData = bufferedInput + header.indiciesArrayPtr;
-            const char* textureData = bufferedInput + header.texturePtr;
-            mesh.nbIndicies = header.indiciesArraySize;
-
-            glGenVertexArrays(1, &mesh.glVAO);
-            glGenBuffers(2, &mesh.glVBO);
-            glGenTextures(1, &mesh.glTexture);
-
-            glBindVertexArray(mesh.glVAO);
-            {
-                glBindBuffer(GL_ARRAY_BUFFER, mesh.glVBO);
-                glBufferData(GL_ARRAY_BUFFER, header.verticiesArraySize * 20, vertexData, GL_STATIC_DRAW);
-                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.glEBO);
-                glBufferData(GL_ELEMENT_ARRAY_BUFFER, header.indiciesArraySize * sizeof(uint32_t), indexData, GL_STATIC_DRAW);
-
-                glEnableVertexAttribArray(0);
-                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, 0);
-                glEnableVertexAttribArray(1);
-                glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 20, (void*) 12);
-            }
-            glBindVertexArray(0);
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-            glBindTexture(GL_TEXTURE_2D, mesh.glTexture);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, header.textureWidth, header.textureHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
-
-            i_lAdd(mesh);
-            goto createMesh;
-        }
-    }
-    else printf("[BIN Mesh Loader][Error]: Unable to load %s\n", filename);
 }
