bc3eb49f0399e0f14b55740d9ac3783789e74f74
elmfrain <elmfer10@gmail.com>
2023-01-30 01:27:51 -0600

    Removed dir directory

--- build/dep/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib
-# Build directory: C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
