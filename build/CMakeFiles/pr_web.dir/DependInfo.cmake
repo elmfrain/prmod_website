14b4e550148fa1a8bc47fc8252fb67edc5f34fa3
elmfrain <elmfer10@gmail.com>
2023-01-30 01:24:41 -0600

    Clear repo

---
+++ build/CMakeFiles/pr_web.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Fern/Downloads/prmod_website/about_view.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/about_view.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/animation.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/animation.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/download_view.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/download_view.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/https_fetcher.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/https_fetcher.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/input.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/input.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/main.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/main.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/markdown_viewer.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/markdown_viewer.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/mesh.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/mesh.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/mesh_builder.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/mesh_builder.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/screen_renderer.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/screen_renderer.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/shaders.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/shaders.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/stb_image.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/stb_image.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/title_view.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/title_view.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/ui_render.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/ui_render.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/widget.c" "C:/Users/Fern/Downloads/prmod_website/build/CMakeFiles/pr_web.dir/widget.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CGLM_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../dep/cglm/include"
+  "../dep/openssl/include"
+  "../dep/assimp/code/../include"
+  "dep/assimp/code/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Fern/Downloads/prmod_website/build/dep/cglm/CMakeFiles/cglm.dir/DependInfo.cmake"
+  "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/code/CMakeFiles/assimp.dir/DependInfo.cmake"
+  "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/adler32.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/adler32.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/compress.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/compress.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/crc32.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/crc32.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/deflate.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/deflate.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/gzclose.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzclose.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/gzlib.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzlib.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/gzread.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzread.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/gzwrite.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzwrite.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/infback.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/infback.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/inffast.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/inffast.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/inflate.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/inflate.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/inftrees.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/inftrees.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/trees.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/trees.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/uncompr.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/uncompr.c.o"
+  "C:/Users/Fern/Downloads/prmod_website/dep/assimp/contrib/zlib/zutil.c" "C:/Users/Fern/Downloads/prmod_website/build/dep/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/zutil.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ASSIMP_BUILD_NO_M3D_EXPORTER"
+  "ASSIMP_BUILD_NO_M3D_IMPORTER"
+  "NO_FSEEKO"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "dep/assimp/include"
+  "dep/assimp"
+  "../dep/assimp/include"
+  "../dep/assimp/code"
+  "../dep/assimp/."
+  "dep/assimp/contrib/zlib"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
